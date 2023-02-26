650eeb7ea757c41771c39ec044be694f50fe744c
Andrey <aba_2007@mail.ru>
2023-02-26 23:42:14 +0300

    Add Random in Example 003

--- Example_003_Sum/Program.cs
+++ Example_003_Sum/Program.cs
@@ -1,3 +1,8 @@
-﻿int a=1;
-int b=2;
-Console.WriteLine(a+b);
+﻿int a = new Random().Next(1, 10);
+Console.Write("Число a = ");
+Console.WriteLine(a);
+int b = new Random().Next(1, 10);
+Console.Write("Число b = ");
+Console.WriteLine(b);
+Console.Write("Сумма чисел = ");
+Console.Write(a+b);
