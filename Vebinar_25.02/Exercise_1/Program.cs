7a68386f6b691986e19810b5846144c43e7cb5a9
Andrey <aba_2007@mail.ru>
2023-02-28 00:24:01 +0300

    Add Homework Exercise 1

---
+++ Vebinar_25.02/Exercise_1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Введите число №1");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число №2");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine("Максимальное число = ");
+    Console.WriteLine(a);
+    Console.WriteLine("Минимальное число = ");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.WriteLine("Максимальное число = ");
+    Console.WriteLine(b);
+    Console.WriteLine("Минимальное число = ");
+    Console.WriteLine(a);
+}
