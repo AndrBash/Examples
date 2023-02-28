48b0901043e72b2665ff2dad2cbc3808c9dc6394
Andrey <aba_2007@mail.ru>
2023-02-28 22:45:47 +0300

    Add Exercise_4 int only one variant

---
+++ Vebinar_25.02/Exercise_4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+{
+    if(a % 2 == 0)
+    {
+        System.Console.WriteLine("Четное число");
+    }    
+   
+    else
+    {
+        System.Console.WriteLine("Нечетное число");
+    }
+}    
