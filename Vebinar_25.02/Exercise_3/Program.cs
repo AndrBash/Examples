fb02c841717b64ff678aaa5d36dad34bc4b65219
Andrey <aba_2007@mail.ru>
2023-02-28 21:30:18 +0300

    Add Exercise_3 int

---
+++ Vebinar_25.02/Exercise_3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Четные числа:");
+for(int i = 1; i <= a; i++)
+{
+    if(i % 2 == 0)
+    {
+        System.Console.WriteLine(i);
+    }    
+   
+    else
+    {
+    i = i++;
+    }
+}    
